67df54f624f22b9a1e87341d484b4c89d33e17d0
LC <lcaccia@matterport.com>
2023-05-27 21:13:53 -0300

    Homepage finished

--- wp/wp-content/themes/hello-elementor/assets/js/mews.js
+++ wp/wp-content/themes/hello-elementor/assets/js/mews.js
@@ -1,24 +1,26 @@
 // Booking Engine Widget load by Luciano Caccia
-// Define element 'radioHotel'
-let radioHotel = document.querySelectorAll('input[name="hotel"]');
 
-// reloadWidget() asign configId from radioHotel input radio and loadWidget()
-function reloadWidget(_radioHotel) {
+
+flatpickr('#calendar-range', {
+    "mode": "range",
+    minDate: "today",
+    dateFormat: "Y-m-d"
+});
+let calendar = document.getElementById('calendar-range');
+
+// Define element 'selectLocation'
+let selectLocation = document.getElementById("selectlocation");
+
+// reloadWidget() asign configId from selectlocation input select and loadWidget()
+function reloadWidget() {
     // Change the submit btn to disabled before loadWidget() 
     const submitButton = document.getElementById('availability');
     submitButton.value = 'Loading...';
     submitButton.disabled = true;
-    for (let i = 0; i < radioHotel.length; i++) {
-        if (radioHotel[i].checked) {
-            let configId = radioHotel[i].value;
-            loadWidget(configId);
-        }
-    }
+    let configId = selectLocation.value;
+    loadWidget(configId);    
 }
 reloadWidget()
-
-// Listen radioHotel changes to reloadWidget()
-radioHotel.forEach(radioHotel => radioHotel.addEventListener('change', () => reloadWidget()));
 
 // Load the widget by the hotel selected asigned in configId
 function loadWidget(configId) {
@@ -32,39 +34,48 @@ function loadWidget(configId) {
             configurationIds: [configId],
             openElements: '.distributor-pepe',
         },
+        
         // Add callback which will enable Submit button and open the Booking Engine Widget upon button click.
         function (api) {
             // Listen on submit and when user submits, open booking engine with given dates.
             const listenOnSubmit = () => {
+                let formOk = false;
                 // Find the form in DOM and listen on submit.
                 const form = document.getElementById('date-form');
                 form.addEventListener('submit', event => {
                     // Don't use the default submit button behavior. We want to handle it ourselves.
-                    event.preventDefault();
-
-                    // Get the dates from the date form.
-                    const { start, end } = event.target.elements;
-                    const [startYears, startMonths, startDays] = start.value.split('-');
-                    const [endYears, endMonths, endDays] = end.value.split('-');
-                    const startDate = new Date(startYears, startMonths - 1, startDays);
-                    const endDate = new Date(endYears, endMonths - 1, endDays);
+                    let val = calendar.value;
+                    if (val != '') {  // If calendar is empty
+                        event.preventDefault();
+                        // Get the dates from the date form.
+                        const { start, end } = event.target.elements;    
+                        const dateRange = calendar.value.split(' to ');
+                        const [startYears, startMonths, startDays] = dateRange[0].split('-');
+                        const [endYears, endMonths, endDays] = dateRange[1].split('-');
+                        const startDate = new Date(startYears, startMonths - 1, startDays);
+                        const endDate = new Date(endYears, endMonths - 1, endDays);
     
-                    // Use the Booking Engine Widget Javascript API to set the dates in the widget and open it.
-                    api.setStartDate(startDate);
-                    api.setEndDate(endDate);
-                    api.setLanguageCode('en-US');
-                    api.setAdultCount(parseInt(adultos.value));
-                    api.setVoucherCode(vouchercode.value);
-                    api.open();
+                        // Use the Booking Engine Widget Javascript API to set the dates in the widget and open it.
+                        api.setStartDate(startDate);
+                        api.setEndDate(endDate);
+                        api.setLanguageCode('en-US');
+                        api.setAdultCount(parseInt(adultos.value));
+                        api.setVoucherCode(vouchercode.value);
+                        api.open();                        
+                    } else { 
+                        // Focus on calendar element if it's empty
+                        document.getElementById('calendar-range').focus();
+                        event.preventDefault();
+                        return
+                    }
                 });
             };
-    
             listenOnSubmit();
     
             // Enable the submit button, because the Booking Engine Widget is ready to be used.
             const enableSubmit = () => {
                 const submitButton = document.getElementById('availability');
-                submitButton.value = 'Check Availability';
+                submitButton.value = 'View Availability';
                 submitButton.disabled = false;
             };
             enableSubmit();
--- wp/wp-content/themes/hello-elementor/footer.php
+++ wp/wp-content/themes/hello-elementor/footer.php
@@ -21,7 +21,11 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 ?>
 
 <?php wp_footer(); ?>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/mews.js"></script>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/mews.css">
+	<!-- Implement Mews Integration -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/mews.js"></script>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/mews.css">
+	<!-- Implement Mews Integration -->
 </body>
 </html>
